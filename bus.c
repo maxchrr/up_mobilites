b953033b088044bd65ba95f49bf99e4969e8f746
Max Charrier <max@puffer.fish>
2025-04-04 18:12:46 +0200

    Getter on Bus_Line_Object
    Signed-off-by: Max Charrier <max@puffer.fish>

--- bus.c
+++ bus.c
@@ -1,2 +1,61 @@
 #include <stdio.h>
 #include "bus.h"
+
+void print_bus_line(Bus_Line_Object* bus_line)
+{
+	if (bus_line == NULL)
+	{
+		printf("This line didn't exist or not allocated.\n");
+		return;
+	}
+	if (bus_line->bus_stop_or_route == BUS_STOP)
+	{
+		printf("Bus Stop %d \"%s\" at (%d,%d)\n",
+			bus_line->id_bus_stop,
+			bus_line->name,
+			bus_line->pos_x,
+			bus_line->pos_y);
+	}
+	else {
+		printf("Bus Route %d (%dm) in %ds\n",
+			bus_line->id_bus_route,
+			bus_line->distance_due,
+			bus_line->time_due);
+		printf("From : "); print_bus_line(bus_line->departure);
+		printf("To : "); print_bus_line(bus_line->arrival);
+		printf("\n");
+	}
+	printf("End of Line\n");
+}
+
+int get_id_bus_stop(Bus_Line_Object* bus_line)
+{
+	return bus_line->id_bus_stop;
+}
+
+char* get_name(Bus_Line_Object* bus_line)
+{
+	return bus_line->name;
+}
+
+int get_pos_x(Bus_Line_Object* bus_line)
+{
+	return bus_line->pos_x;
+}
+
+int get_pos_y(Bus_Line_Object* bus_line)
+{
+	return bus_line->pos_y;
+}
+
+int get_id_bus_route(Bus_Line_Object* bus_line)
+{
+	if (bus_line->bus_stop_or_route == BUS_STOP)
+		return -1;
+	return bus_line->id_bus_route;
+}
+
+Bus_Line_Object_Type get_type(Bus_Line_Object* bus_line)
+{
+	return bus_line->bus_stop_or_route;
+}
--- bus.h
+++ bus.h
@@ -16,7 +16,7 @@ typedef struct
 
 typedef struct
 {
-	Bus_Line_Object_Type bus_stop_or_line;
+	Bus_Line_Object_Type bus_stop_or_route;
 
 	/* Type représentant un arrêt */
 	int id_bus_stop;
@@ -24,16 +24,25 @@ typedef struct
 	int pos_x, pos_y;
 
 	/* Type représentant un tronçon */
-	int id_bus_line;
+	int id_bus_route;
 	struct Bus_Line_Object* departure;
 	struct Bus_Line_Object* arrival;
-	int weight_distance;         // Coût en distance (mètres)
-	int weight_time;             // Coût en temps de parcours (secondes)
-	int distance_due, time_due;  // Algorithme de Dijkstra
+	int distance_due; // Coût en distance (mètres)
+	int time_due;     // Coût en temps de parcours (secondes)
+	int weight_distance, weight_time; // Algorithme de Dijkstra
 
 	/* Pour les tris */
 	int maintenance_price;
 	Maintenance_Date maintenance_date;
 } Bus_Line_Object;
+
+void print_bus_line(Bus_Line_Object* bus_line);
+
+int get_id_bus_stop(Bus_Line_Object* bus_line);
+char* get_name(Bus_Line_Object* bus_line);
+int get_pos_x(Bus_Line_Object* bus_line);
+int get_pos_y(Bus_Line_Object* bus_line);
+int get_id_bus_route(Bus_Line_Object* bus_line);
+Bus_Line_Object_Type get_type(Bus_Line_Object* bus_line);
 
 #endif // BUS_H_
