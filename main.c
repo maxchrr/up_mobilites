ee7f00191f3427ef9baac41bc15e06e08a7678cd
Max Charrier <max@puffer.fish>
2025-04-07 19:50:13 +0200

    Make code as a function
    Signed-off-by: Max Charrier <max@puffer.fish>

--- main.c
+++ main.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
-#include "list.h"
+#include "bus.h"
 #include "sort.h"
 #include "utils.h"
 
-int main(void)
+List_Bus_Line create_bus_line(void)
 {
-	List_Bus_Line line;
+	List_Bus_Line new_line;
 	Bus_Line_Object* departure;
 	Bus_Line_Object* arrival;
 	Bus_Line_Object* route;
-	init_list(&line);
+	init_list(&new_line);
 
 	departure = create_stop(1,"Foo",0,0);
 	arrival = create_stop(2,"Bar",10,2);
 	route = create_route(1,departure,arrival,280,35);
 
-	line = insert_at_tail(line, departure);
-	line = insert_at_tail(line, route);
-	line = insert_at_tail(line, arrival);
+	new_line = insert_at_tail(new_line, departure);
+	new_line = insert_at_tail(new_line, route);
+	new_line = insert_at_tail(new_line, arrival);
 
 	departure = create_stop(3,"Baz",20,4);
 	route = create_route(1,arrival,departure,160,40);
-	line = insert_at_tail(line, route);
-	line = insert_at_tail(line, departure);
+	new_line = insert_at_tail(new_line, route);
+	new_line = insert_at_tail(new_line, departure);
 
 	arrival = create_stop(4,"Qux",30,6);
 	route = create_route(1,departure,arrival,200,45);
-	line = insert_at_tail(line, route);
-	line = insert_at_tail(line, arrival);
+	new_line = insert_at_tail(new_line, route);
+	new_line = insert_at_tail(new_line, arrival);
 
 	departure = create_stop(5,"Quux",40,8);
 	route = create_route(1,arrival,departure,160,40);
-	line = insert_at_tail(line, route);
-	line = insert_at_tail(line, departure);
-	print_list(line);
+	new_line = insert_at_tail(new_line, route);
+	new_line = insert_at_tail(new_line, departure);
+
+	return new_line;
+}
+
+int main(void)
+{
+	List_Bus_Line bus_line = create_bus_line();
+	Bus_Object bus = create_bus(1, bus_line);
+	print_bus_line(bus_line);
 	return 0;
 }
