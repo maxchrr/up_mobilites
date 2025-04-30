064c13f9e35355af7ff1bddd9256457f36bc3602
Max Charrier <max@puffer.fish>
2025-04-30 13:50:24 +0200

    Center label
    Signed-off-by: Max Charrier <max@puffer.fish>

--- sources/ui.c
+++ sources/ui.c
@@ -59,7 +59,8 @@ void draw_bl(List l, Font font, Color color)
 			DrawCircle(dx, dy, 8+4,	BLACK);
 			DrawCircle(dx, dy, 8+2,	WHITE);
 			DrawCircle(dx, dy, 8,	color);
-			Vector2 labelPos = { dx-16-8*2, dy-16-8*2 };
+			Vector2 labelSize = MeasureTextEx(font, bs_getname(s), 16, 0);
+			Vector2 labelPos = { dx - labelSize.x / 2, dy - labelSize.y / 2 - 24 };
 			DrawTextEx(font, bs_getname(s), labelPos, 16, 0, BLACK);
 		}
 		temp = _get_next_node(temp);
