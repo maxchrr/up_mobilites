70f92392cf42ed6c474ddbc3c6c23746911cd7f0
Max Charrier <max@puffer.fish>
2025-04-30 10:56:37 +0200

    Implement generic function to init bus line
    Signed-off-by: Max Charrier <max@puffer.fish>

--- sources/main.c
+++ sources/main.c
@@ -13,96 +13,68 @@
 #define SCREEN_HEIGHT	800
 #define WINDOW_TITLE	"UPmobilites"
 
-List create_bl1(const int id)
+List init_bl(int id, const char* names[], int positions[][2], int count)
 {
-	BusStation* d;
-	BusStation* a;
-	BusRoute* r;
 	List new_bl;
 	init_list(&new_bl);
-	d = create_bs(1, "Autoroute", 100, 100);
-	a = create_bs(2, "Rivière", 300, 100);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	d = create_bs(3, "Centre ville", 500, 100);
-	r = create_br(id, a, d);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	a = create_bs(4, "Université", 700, 100);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	d = create_bs(5, "Ville Sud", 900, 100);
-	r = create_br(id, a, d);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
+	BusStation* cs = NULL;
+	BusStation* ps = NULL;
+	for (int i=0; i<count; ++i)
+	{
+		cs = create_bs(i+1, names[i], positions[i][0], positions[i][1]);
+		if (i > 0)
+		{
+			BusRoute* r = create_br(id, ps, cs);
+			new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
+		}
+		new_bl = insert_at_tail(new_bl, open_entity(STATION, cs));
+		ps = cs;
+	}
 	return new_bl;
 }
 
-List create_bl2(const int id)
+List create_bl1(int id)
 {
-	BusStation* d;
-	BusStation* a;
-	BusRoute* r;
-	List new_bl;
-	init_list(&new_bl);
-	d = create_bs(1, "Maison Royale", 400, 0);
-	a = create_bs(2, "Place du marché", 400, 150);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	d = create_bs(3, "Gare", 400, 300);
-	r = create_br(id, a, d);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	a = create_bs(4, "Téléport", 400, 450);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	return new_bl;
+	const char* names[] = {
+		"Autoroute", "Rivière", "Centre ville", "Université", "Ville Sud"
+	};
+	int positions[][2] = {
+		{100, 100}, {300, 100}, {500, 100}, {700, 100}, {900, 100}
+	};
+	return init_bl(id, names, positions, 5);
 }
 
-List create_bl3(const int id)
+List create_bl2(int id)
 {
-	BusStation* d;
-	BusStation* a;
-	BusRoute* r;
-	List new_bl;
-	init_list(&new_bl);
-	d = create_bs(1, "Zone Industrielle", 220, -150);
-	a = create_bs(2, "Déchetterie", 220, -50);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	d = create_bs(3, "Ateliers Nord", 220, 50);
-	r = create_br(id, a, d);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	a = create_bs(4, "Quartier Est", 220, 150);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	d = create_bs(5, "Centrale", 220, 250);
-	r = create_br(id, a, d);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	a = create_bs(6, "Parc Logistique", 100, 250);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	d = create_bs(7, "Cimetière", 100, 350);
-	r = create_br(id, a, d);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, d));
-	a = create_bs(8, "Centre Hospitalier", 100, 450);
-	r = create_br(id, d, a);
-	new_bl = insert_at_tail(new_bl, open_entity(ROUTE, r));
-	new_bl = insert_at_tail(new_bl, open_entity(STATION, a));
-	return new_bl;
+	const char* names[] = {
+		"Maison Royale", "Place du marché", "Gare", "Téléport"
+	};
+	int positions[][2] = {
+		{400, 0}, {400, 150}, {400, 300}, {400, 450}
+	};
+	return init_bl(id, names, positions, 4);
+}
+
+List create_bl3(int id)
+{
+	const char* names[] = {
+		"Zone Industrielle", "Déchetterie", "Ateliers Nord", "Quartier Est", "Centrale", "Parc Logistique", "Cimetière", "Centre Hospitalier"
+	};
+	int positions[][2] = {
+		{220, -150}, {220, -50}, {220, 50}, {220, 150}, {220, 250}, {100, 250}, {100, 350}, {100, 450}
+	};
+	return init_bl(id, names, positions, 8);
+}
+
+List create_bl4(int id)
+{
+	const char* names[] = {
+		"Zone Nord", "Piscine", "Hôtel de Ville", "Musée", "Stade"
+	};
+	int positions[][2] = {
+		{0, 200}, {250, 200}, {500, 200}, {750, 200}, {1000, 200}
+	};
+	return init_bl(id, names, positions, 5);
 }
 
 int main(void)
@@ -110,11 +82,15 @@ int main(void)
 	List bl1 = create_bl1(101);
 	List bl2 = create_bl2(102);
 	List bl3 = create_bl3(103);
+	List bl4 = create_bl4(104);
 	BusPtr bus1 = init_bus(1, bl1); // Bus sur ligne 1
 	BusPtr bus2 = init_bus(2, bl2); // Bus sur ligne 2
+	BusPtr bus3 = init_bus(3, bl3); // Bus sur ligne 3
+	BusPtr bus4 = init_bus(4, bl4); // Bus sur ligne 4
 	print_list(bl1);
 	print_list(bl2);
 	print_list(bl3);
+	print_list(bl4);
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
 	SetTargetFPS(60);
@@ -123,9 +99,13 @@ int main(void)
 
 	ClearBackground(RAYWHITE);
 	draw_bus(bus1, YELLOW);
-	draw_bus(bus2, YELLOW);
+	draw_bus(bus2, ORANGE);
+	draw_bus(bus3, GOLD);
+	draw_bus(bus4, PINK);
 	int incx1 = 0, incy1 = 0;
 	int incx2 = 0, incy2 = 0;
+	int incx3 = 0, incy3 = 0;
+	int incx4 = 0, incy4 = 0;
 
 	while (!WindowShouldClose())
 	{
@@ -135,11 +115,16 @@ int main(void)
 		draw_bl(bl1, font, DARKGREEN);
 		draw_bl(bl2, font, DARKBLUE);
 		draw_bl(bl3, font, DARKPURPLE);
+		draw_bl(bl4, font, DARKBROWN);
 
 		draw_bus(bus1, YELLOW);
 		bus_travel(bus1, bus_getdirection(bus1), &incx1, &incy1);
 		draw_bus(bus2, ORANGE);
 		bus_travel(bus2, bus_getdirection(bus2), &incx2, &incy2);
+		draw_bus(bus3, GOLD);
+		bus_travel(bus3, bus_getdirection(bus3), &incx3, &incy3);
+		draw_bus(bus4, PINK);
+		bus_travel(bus4, bus_getdirection(bus4), &incx4, &incy4);
 
 		EndDrawing();
 	}
@@ -151,5 +136,6 @@ int main(void)
 	destroy_list(bl1);
 	destroy_list(bl2);
 	destroy_list(bl3);
+	destroy_list(bl4);
 	return 0;
 }
